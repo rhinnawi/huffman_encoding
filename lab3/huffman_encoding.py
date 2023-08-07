1bda7e224ad8914b3f4c6651dee3bb9409ac5a3e
Rani Hinnawi <rhinnawi95@gmail.com>
2023-08-07 15:46:25 -0400

    added encoding with memoization

--- lab3/huffman_encoding.py
+++ lab3/huffman_encoding.py
@@ -13,6 +13,9 @@ from typing import List, Optional
 from lab3.huffman_tree import HuffmanTree
 from lab3.huffman_node import HuffmanNode
 
+ALLOWED_PUNCTUATION = {'.', ',', ';', ':', '!', '?', '-',
+                       '"', "'", '(', ')', '/', '\\', '_', '@', '&', '*', '~'}
+
 
 class HuffmanEncoding:
     """
@@ -20,9 +23,15 @@ class HuffmanEncoding:
     the frequency values per character it contains.
     """
 
-    def __init__(self, huffman_tree: 'HuffmanTree') -> 'HuffmanEncoding':
+    def __init__(self, huffman_tree: 'HuffmanTree',
+                 allowed_nonalpha_chars=None) -> 'HuffmanEncoding':
         self._tree = huffman_tree
-        self._set_codes()
+        self._allowed_nonalpha_chars = allowed_nonalpha_chars if \
+            allowed_nonalpha_chars is not None else ALLOWED_PUNCTUATION
+        # Set binary codes for quick retrieval if has memo. Otherwise, find
+        # dynamically during encoding process
+        if self._tree.has_memo():
+            self._set_codes()
 
     def __str__(self) -> str:
         """
@@ -87,11 +96,25 @@ class HuffmanEncoding:
 
         return self
 
-    def encode(self, expression: str) -> str:
+    def _value_error_message(self, char: str) -> str:
+        """
+        Helper method for standardizing value error message across both encode
+        methods.
+
+        Args:
+            char (str): invalid character referenced in error message
+        """
+        error = f"INVALID CHAR: the character '{char}' is not a leaf node in "
+        error += "the frequency table, a permitted punctuation symbol, or a "
+        error += "whitespace"
+        return error
+
+    def _encode_with_memo(self, expression: str) -> str:
         """
         Encodes a given expression using the Huffman Tree. Each character
         should appear as a leaf node, which carries a binary Huffman code
-        corresponding to it.
+        corresponding to it. Uses memoization to quickly call a reference to
+        the node and retrieve pre-calculated code.
 
         Args:
             expression (str): the string being encoded
@@ -103,28 +126,90 @@ class HuffmanEncoding:
             ValueError: when a non-punctuation or non-white space character 
                 appears that is not a leaf node in the Huffman Tree (it has no
                 corresponding Huffman code)
-
-        TODO: Implement
         """
-        def encode_char(node: 'HuffmanNode', char: str, code=""):
+        encoded = ""
+        leaf_nodes = self._tree.get_memo()
+        for char in expression:
+            # Get binary encoding for symbol. Add to output string
+            if (char in self._allowed_nonalpha_chars) or (char.isspace()):
+                # Case: char is a whitespace or permitted punctuation symbol
+                continue
+
+            index = ord(char) - ord('A')
+            if (index >= len(leaf_nodes)) or (index < 0):
+                # Error case: character is not in the Huffman tree
+                raise ValueError(self._value_error_message(char))
+
+            node = leaf_nodes[index]
+            if node is not None:
+                encoded += node.get_code()
+            else:
+                # Error case: character is not in the Huffman tree
+                raise ValueError(self._value_error_message(char))
+
+        return encoded
+
+    def _encode_without_memo(self, expression: str) -> str:
+        """
+        Encodes a given expression using the Huffman Tree. Each character
+        should appear as a leaf node, which carries a binary Huffman code
+        corresponding to it. Uses preorder traversal to dynamically calculate
+        Huffman code.
+
+        Args:
+            expression (str): the string being encoded
+
+        Returns:
+            str: a new binary string made entirely of 1s and 0s
+
+        Raises:
+            ValueError: when a non-punctuation or non-white space character 
+                appears that is not a leaf node in the Huffman Tree (it has no
+                corresponding Huffman code)
+        """
+        def encode_char(node: 'HuffmanNode', target_letter: str, code="") \
+                -> Optional[str]:
+            """
+            Helper function for leveraging preorder traversal to determine a
+            character's Huffman code.
+
+            Args:
+                node (HuffmanNode): Root of Huffman Tree or subtree
+                char (str): character being searched for in Huffman Tree
+                code (str): binary Huffman code of parent node
+
+            Returns:
+                str: Binary number indicating position of char in the Huffman
+                    Tree OR None if not in tree
+            """
             if node.is_leaf():
-                if node.get_characters() == char:
+                if node.get_characters() == target_letter:
                     return code
                 else:
                     return None
             else:
-                left_code = encode_char(node.get_left(), char, code + "0")
+                # Must be leaf. Search left, then search right for char
+                left_code = encode_char(node.get_left(), target_letter,
+                                        code + "0")
                 if left_code is not None:
                     return left_code
-                right_code = encode_char(node.get_right(), char, code + "1")
+
+                right_code = encode_char(node.get_right(), target_letter,
+                                         code + "1")
                 return right_code
 
         encoded = ""
         for char in expression:
-            char_code = encode_char(self._tree.get_root(), char)
-            if char_code is not None:
-                encoded += char_code
+            # Get binary encoding for symbol. Add to output string
+            if (char in self._allowed_nonalpha_chars) or (char.isspace()):
+                # Case: char is a whitespace or permitted punctuation symbol
+                continue
+
+            letter_code = encode_char(self._tree.get_root(), char)
+            if letter_code is not None:
+                encoded += letter_code
             else:
-                # Handle the case where the character is not in the Huffman tree
-                pass
-        return ""
+                # Error case: character is not in the Huffman tree
+                raise ValueError(self._value_error_message(char))
+
+        return encoded
